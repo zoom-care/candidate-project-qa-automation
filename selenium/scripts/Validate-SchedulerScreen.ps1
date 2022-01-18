763e6b618c26482aeebfa32d4d0ca7ec12fbbf63
Ryan Joachim <ryanjoachim@gmail.com>
2022-01-18 02:10:49 -0800

    Update Validate-SchedulerScreen.ps1
    Simplified script, added logging, and added result validation.

--- selenium/scripts/Validate-SchedulerScreen.ps1
+++ selenium/scripts/Validate-SchedulerScreen.ps1
@@ -1,18 +1,13 @@
-# Your working directory
-$workingPath = "$PSScriptRoot\selenium"
+# Logging
+Start-Transcript -Path "$Tests\$CurrentScript.txt" -Force -NoClobber -Append -Verbose
 
-# Import both WebDriver libraries
-Import-Module "$($workingPath)\WebDriver.dll"
-Import-Module "$($workingPath)\WebDriver.Support.dll"
-
-# Start Chrome and go to URL
-$ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+# Go to URL
 $ChromeDriver.Navigate().GoToURL('https://www.zoomcare.com/schedule')
 Start-Sleep -Seconds 10 # Give the page time to load
 
 # Verify the scheduling element is visible
 $SchedulerScreen = $ChromeDriver.FindElement([OpenQA.Selenium.By]::XPath('//*[@id="react-mount-page-content"]/div/div/div/div[2]/div/div[2]/div'))
-($SchedulerScreen.Displayed)
 
-$ChromeDriver.Close()
-$ChromeDriver.Quit()
+$Result = ($SchedulerScreen.Displayed)
+
+Stop-Transcript
